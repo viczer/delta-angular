82c186bfbb2678f0dfe4d4d4cf9c23b8c201f4c3
Miguel Romero <miguelromereado@outlook.com>
2020-01-28 06:13:09 -0600

    flights

--- src/app/pages/flights/flight/flight.component.ts
+++ src/app/pages/flights/flight/flight.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { IFlight } from "src/app/interfaces/flight.interface";
+import { IUser } from "src/app/interfaces/user.interface";
+import { ActivatedRoute } from "@angular/router";
+import { StudentService } from "src/app/services/student.service";
+import { FlightsService } from "src/app/services/flights.service";
+import { IResponse } from "src/app/interfaces/response.interface";
 
 @Component({
-  selector: 'app-flight',
-  templateUrl: './flight.component.html',
-  styleUrls: ['./flight.component.scss']
+  selector: "app-flight",
+  templateUrl: "./flight.component.html",
+  styleUrls: ["./flight.component.scss"]
 })
-export class FlightComponent implements OnInit {
+export class FlightComponent {
+  public flight: IFlight = {};
+  public flightMembers: IUser[] = [];
+  public students: IUser[] = [];
+  public id: string;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private studentService: StudentService,
+    private flightsService: FlightsService
+  ) {
+    this.activatedRoute.params.subscribe(params => {
+      this.id = params["id"];
 
-  constructor() { }
+      this.studentService.findAll().subscribe((response: IResponse) => {
+        this.students = response.data;
+      });
 
-  ngOnInit() {
+      this.flightsService.findById(this.id).subscribe((response: IResponse) => {
+        this.flight = response.data;
+      });
+    });
   }
+  handleReload() {
+    setTimeout(() => {
+      this.activatedRoute.params.subscribe(params => {
+        this.id = params["id"];
 
+        this.studentService.findAll().subscribe((response: IResponse) => {
+          this.students = response.data;
+        });
+
+        this.flightsService
+          .findById(this.id)
+          .subscribe((response: IResponse) => {
+            this.flight = response.data;
+          });
+      });
+    }, 300);
+  }
 }
